8927ed6e77a0359e182c6e47e32b4e45f2ccdf21
arjun15m <114432658+arjun15m@users.noreply.github.com>
2023-06-16 17:04:25 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,97 @@
+var userClickedPattern = [];
+
+var gamePattern = [];
+
+var buttonColours = ["red", "blue" , "green", "yellow"];
+
+var started = false;
+
+var level = 0;
+
+$(document).keypress(function(){
+ if(!started){
+
+    $("#level-title").text("Level "+level);
+
+    nextSequence();
+    started = true;
+ }
+});
+
+
+function nextSequence(){
+
+    userClickedPattern = [];
+
+level++;
+
+    $("#level-title").text("Level "+level);
+
+    var randomNumber = Math.floor(Math.random()*4) ;
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    
+   playSound(randomChosenColour);
+
+}
+
+$(".btn").click(handler);
+
+function handler(){
+ var userChosenColour = $(this).attr("id");
+ userClickedPattern.push(userChosenColour);
+
+ playSound(userChosenColour);
+ animatePress(userChosenColour);
+
+ checkAnswer(userClickedPattern.length-1);
+}
+
+function playSound(name){
+    var audio = new Audio(name+".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour){
+    $("#"+currentColour).addClass("pressed");
+    
+    setTimeout(rem , 100);
+
+    function rem(){
+        $("#"+currentColour).removeClass("pressed");
+    }
+}
+
+function checkAnswer(currentLevel){
+  if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+    console.log("success");
+  
+
+  if(userClickedPattern.length === gamePattern.length){
+    setTimeout(function () {
+        nextSequence();
+      }, 1000);
+  }
+}
+else{
+    console.log("wrong");
+    playSound("wrong");
+
+    $("body").addClass("game-over");
+
+    setTimeout(function(){
+        $("body").removeClass("game-over");
+    }, 200);
+
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    startOver();
+}
+}
+
+function startOver(){
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
